115eac83aaee87a8d19fcd72eea0ed337c721f30
Richierosecodes <roselynolayinkaakintobi@gmail.com>
2024-04-08 21:38:58 +0100

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
+function displayPoem(response) {
+  console.log("poem generated");
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+
+}
+
+
+
+
 function generatePoem(event) {
     event.preventDefault();
 
-    let poemElement=document.querySelector("#poem");
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "cf4b8d4et700d15264c45doca5caf311";
+    let prompt = `Generate a poem about ${instructionsInput.value}`;
+    let context = "You are a romantic poem expert and you love to write short poems. Your mission is to generate a-4 line poem in basic HTML and separate each line with <br />. Make sure you follow the user instruction. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    new Typewriter("#poem", {
-        strings: "Les Fleurs du Mal",
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
+    console.log("Generating poem");
+    console.log(`prompt: ${prompt}`);
+    console.log(`context: ${context}`);
+
+
+    axios.get(apiUrl).then(displayPoem);
+    
+
+   
 }
 
 
